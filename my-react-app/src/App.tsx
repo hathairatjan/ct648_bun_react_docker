01a672f8422bdc6665a78423f3cc2cfc2b8cdecd
Saipan <102419452+hathairatjan@users.noreply.github.com>
2024-09-21 13:32:38 +0700

    Update App.tsx

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,12 +2,12 @@ import "./App.css";
 import DataDisplay from "./components/DataDisplay";
 import DataDisplay_pok from "./components/DataDisplay_pok";
 // import PokemonTable from "./components/PokemonTable";
-import PokemonBackground from "./components/PokemonBackground"; // นำเข้าคอมโพเนนต์ Pokémon Background
+import PokemonBackground from "./components/PokemonBackground"; 
 
 function App() {
   return (
     <div className="app-container">
-      <PokemonBackground /> {/* เพิ่มพื้นหลัง Pokémon */}
+      <PokemonBackground /> 
       <div className="grid-container">
         <div className="left-column">
           <h1>Pokémon Random</h1>
